ef4f3069278173de966f5aadd589e909b39f065a
David M Vermillion <social@davidmvermillion.com>
2022-04-07 16:29:29 -0600

    Update data_exploration.R

--- data_exploration.R
+++ data_exploration.R
@@ -2,7 +2,12 @@
 library(tidyverse)
 library(rjson)
 
+
 # Load data
 htdata <- fromJSON(file = '9780.json')
 
-# Data unusuable. I'll need to get something else.
+# Un-nest data
+# https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
+ht2 <- enframe(unlist(htdata))
+
+# Continue importimg, then find way to sort and look for interesting info from metadata.
