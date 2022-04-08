047ae0726d0873f7beac652fa3b88ffb3f0ef904
David M Vermillion <social@davidmvermillion.com>
2022-04-08 17:12:37 -0600

    Working on automating the data import step

--- data_exploration.R
+++ data_exploration.R
@@ -1,10 +1,19 @@
 # Initialize workspace
 library(tidyverse)
 library(rjson)
+library(data.table)
+library(rio)
 
 
 # Load data
-htdata <- fromJSON(file = '9780.json')
+# https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
+https://stackoverflow.com/questions/35421870/reading-multiple-json-files-in-a-directory-into-one-data-frame
+htdata <- fromJSON(file = 'data/9780.json')
+htdata2 <- 
+  list.files(path="data", pattern = "*.json", full.names = T) %>% 
+  map_df(~fromJSON(.))
+htdata3 <- import_list(dir(path = "data", pattern = ".json"), rbind = TRUE)
+htdata4 <- import(dir(path = "data", pattern = ".json"))
 
 # Un-nest data
 # https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
