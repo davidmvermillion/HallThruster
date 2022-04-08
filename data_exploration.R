424b314f4114a0b378f58c0ab97c39f13b064e68
David M Vermillion <social@davidmvermillion.com>
2022-04-08 17:18:09 -0600

    Trying more ways to automate import

--- data_exploration.R
+++ data_exploration.R
@@ -3,17 +3,29 @@ library(tidyverse)
 library(rjson)
 library(data.table)
 library(rio)
+library(purrr)
+library(jsonlite)
 
 
 # Load data
 # https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
-https://stackoverflow.com/questions/35421870/reading-multiple-json-files-in-a-directory-into-one-data-frame
+# https://stackoverflow.com/questions/35421870/reading-multiple-json-files-in-a-directory-into-one-data-frame
 htdata <- fromJSON(file = 'data/9780.json')
 htdata2 <- 
   list.files(path="data", pattern = "*.json", full.names = T) %>% 
   map_df(~fromJSON(.))
 htdata3 <- import_list(dir(path = "data", pattern = ".json"), rbind = TRUE)
 htdata4 <- import(dir(path = "data", pattern = ".json"))
+path <- "data"
+files <- dir(path, pattern = "*.json")
+
+data <- files %>% 
+  map_df(~fromJSON(file.path(path, .), flatten = TRUE))
+
+data <- dir(path, pattern = "*.json") %>% 
+  map_df(~fromJSON(file.path(path, .), flatten = TRUE))
+
+htdata5 <- 
 
 # Un-nest data
 # https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
