669a59caf16ba1da05c73a542669d0ea2c2b1d53
David M Vermillion <social@davidmvermillion.com>
2022-04-10 10:32:02 -0600

    Fixed path
    Fully relative now

--- data_exploration.R
+++ data_exploration.R
@@ -8,7 +8,7 @@ library(fs)
 # https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
 # https://stackoverflow.com/questions/35421870/reading-multiple-json-files-in-a-directory-into-one-data-frame
 # https://githubhot.com/repo/r-lib/fs/issues/227
-htdata <- map(dir_ls(path = paste0(getwd(), "/data")), read_json)
+htdata <- map(dir_ls(path = "data"), read_json)
 
 # Un-nest data
 # https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
@@ -16,7 +16,7 @@ ht2 <- enframe(unlist(htdata))
 
 # Remove path
 # https://reactgo.com/r-remove-first-n-characters-string/
-ht2$name <- substring(ht2$name, 51)
+ht2$name <- substring(ht2$name, 6)
 
 # Still need to find a way to filter the names to something useful
 
