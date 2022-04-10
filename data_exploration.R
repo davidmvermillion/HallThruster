51c0615cf5b02d56f222336e32f09671052b0feb
David M Vermillion <social@davidmvermillion.com>
2022-04-09 19:54:18 -0600

    Removed path from variable names

--- data_exploration.R
+++ data_exploration.R
@@ -4,7 +4,6 @@ library(purrr)
 library(jsonlite)
 library(fs)
 
-
 # Load data
 # https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
 # https://stackoverflow.com/questions/35421870/reading-multiple-json-files-in-a-directory-into-one-data-frame
@@ -14,5 +13,11 @@ htdata <- map(dir_ls(path = paste0(getwd(), "/data")), read_json)
 # Un-nest data
 # https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
 ht2 <- enframe(unlist(htdata))
+
+# Remove path
+# https://reactgo.com/r-remove-first-n-characters-string/
+ht2$name <- substring(ht2$name, 51)
+
+# Still need to find a way to filter the names to something useful
 
 # Continue importing, then find way to sort and look for interesting info from metadata.
